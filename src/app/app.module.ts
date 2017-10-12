98347b40c5270d02519a01ba3d63640f6ee317bb
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-12 12:04:49 -0400

    Registration only assignment using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // <-- 
+import { HttpModule } from '@angular/http';
+import { MyComponentComponent } from './my-component/my-component.component'; // <-- 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // <-- Include module in our AppModules
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
